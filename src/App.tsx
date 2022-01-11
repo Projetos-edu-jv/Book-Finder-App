e315e3d284da3d6c792a65c162f4ec8d16bf6aa4
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2022-01-11 10:34:04 -0300

    sla

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
+export default function App()
+{
+  const [ data, setData ]: Array<any> = useState();
+  const [ load, setLoad ] = useState(false);
+  const [ pesquisa, setPesquisa ] = useState("");
+  const [ pesq, setPesq] = useState('');
+
+  useEffect( () =>
+  {
+    Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${pesq && pesq || pesq == '' && 'finanças'}&orderBy=relevance`)
+      .then((res) => setData(res.data.items)
+    );
+
+  }, [pesq]);
+
+  if (data != undefined && data){
+    data.map((data:any) => {
+      console.log(data);
+      
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container">
+          <h2>Pesquisa de livros</h2>
+          <label htmlFor="book">Nome do livro: </label>
+          <input type="text" name="book" id="book" onChange={ (e) => setPesquisa(e.target.value) }/>
+          <button onClick={ () => setPesq(pesquisa) }>Pesquisar?</button>
 
-export default App;
+          { data !== undefined &&
+            <div className="lista">
+            {data.map((data: any) => {
+              return (
+              <div>
+                <h1>{data.volumeInfo.title}</h1>
+                {data.volumeInfo.subtitle && <h2>data.volumeInfo.subtitle</h2>}
+              </div>
+              )
+            })}
+            </div>
+          }
+
+          { !data && <h3>Nenhum livro pesquisado ainda!</h3> }
+      </div>
+    </>
+  );
+};
