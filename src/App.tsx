2aa880d4ed0c94afe69213a629836b87f4704869
Jvsm123 <Jvsm123@outlook.com>
2022-01-13 22:16:54 -0300

    Novas alterações

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,52 @@ import Axios from 'axios';
 
 export default function App()
 {
-  const [ data, setData ]: Array<any> = useState();
-  const [ load, setLoad ] = useState(false);
-  const [ pesquisa, setPesquisa ] = useState("");
-  const [ pesq, setPesq] = useState('');
+	const [ data, setData ]: Array<any> = useState();
+	//const [ load, setLoad ] = useState(false);
+	const [ pesquisa, setPesquisa ] = useState("");
+	const [ pesq, setPesq] = useState('');
 
-  useEffect( () =>
-  {
-    Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${pesq && pesq || pesq == '' && 'finanças'}&orderBy=relevance`)
-      .then((res) => setData(res.data.items)
-    );
+	useEffect( () =>
+	{
+		Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${pesq && pesq || pesq == '' && 'finanças'}&orderBy=relevance`)
+		.then( (res) =>
+		{
+			setData(res.data.items);
+			console.log( res.data.items[0].volumeInfo.imageLinks.thumbnail );
+		});
+	}, [pesq]);
 
-  }, [pesq]);
+	return (
+		<>
+			<div className="container">
 
-  if (data != undefined && data){
-    data.map((data:any) => {
-      console.log(data);
-      
-    })
-  }
-  
-  return (
-    <>
-      <div className="container">
-          <h2>Pesquisa de livros</h2>
-		  <div className="main-header">
-		      <input placeholder="Nome do livro" type="text" name="book" id="book" onChange={ (e) => setPesquisa(e.target.value) }/>
-			  <button onClick={ () => setPesq(pesquisa) }>Pesquisar</button>
-		  </div>
+				<div className="main-header">
+					<h2>Pesquisa de livros</h2>
 
-          { data !== undefined &&
-            <div className="lista">
-            { data.map( (data: any) => {
-              return (
-              <div className="card">
-                <h3>{ data.volumeInfo.title }</h3>
-                {data.volumeInfo.subtitle && <h4>data.volumeInfo.subtitle</h4>}
-              </div>
-              )
-            })}
-            </div>
-          }
+					<input placeholder="Nome do livro" type="text" name="book" id="book" onChange={ (e) => setPesquisa(e.target.value) }/>
 
-          { !data && <h3>Nenhum livro pesquisado ainda!</h3> }
-      </div>
-    </>
-  );
+					<button onClick={ () => setPesq(pesquisa) }>Pesquisar</button>
+				</div>
+
+				{ data !== undefined &&
+					<div className="lista">
+					{
+						data.map( (data: any) =>
+						{
+							return (
+								<div className="card">
+									<h3>{ data.volumeInfo.title }</h3>
+									{ data.volumeInfo.subtitle && <h4>{data.volumeInfo.subtitle}</h4> }
+									{ data.volumeInfo.imageLinks && <img src={data.volumeInfo.imageLinks.thumbnail} alt="IMAGEM" />}
+								</div>
+							)
+						})
+					}
+					</div>
+				}
+
+				{ !data && <h3>Nenhum livro pesquisado ainda!</h3> }
+			</div>
+		</>
+	);
 };
